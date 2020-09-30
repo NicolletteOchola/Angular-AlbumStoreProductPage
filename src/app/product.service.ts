6af907e220094f6ffd7a917f409f361075f44aed
NicolletteOchola <nicoleochola@egmail.com>
2020-09-30 18:49:24 +0300

    Corrected changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,8 +15,8 @@ export class ProductService {
 
   constructor(private _http: Http) {}
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json(<Album>));
-    return Observable<Album>
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+    
   }
 }
